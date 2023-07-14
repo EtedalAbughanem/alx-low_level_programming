aa55a8b3c2b57b7bbd3fcbba5af7cf91baa30ceb
Dolly <dollyabughanem@gmail.com>
2023-07-13 17:31:20 -0700

    Added Scripts and programs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Description: C program that uses printf function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
