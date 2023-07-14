5b35ec361b2283c9fa7b698e3fabb2f5e13007c1
Dolly <dollyabughanem@gmail.com>
2023-07-13 17:37:36 -0700

    Added Scripts and programs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#include<stidio.h>
-
+#include <stdio.h>
 /**
- *main -Entry point
- *
- * Description: A C program that prints with puts function
- *
- * Return: Always 0 (Success)
-*/
-
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
